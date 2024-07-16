76db21f93f0a2145d9ec20546dd60701449a496a
thanhtoan1705 <153188343+thanhtoan1705@users.noreply.github.com>
2024-07-16 19:23:15 +0700

    init src

---
+++ app/Http/Controllers/Clients/Pages/CandidateController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Clients\Pages;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CandidateController extends Controller
+{
+    public function index(){
+        return view();
+    }
+}
